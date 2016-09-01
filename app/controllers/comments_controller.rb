d8da743ba961f5cee8877ed8f5f9217c3d471433
Akhmedov Daniiar <lakiwolf.90@mail.ru>
2016-09-01 15:01:22 +0600

    Created comments_controller and actions new create index welcom

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,10 @@
+class CommentsController < ApplicationController
+  def welcom
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get 'comments/new'
+
+  # get 'comments/create'
+
+  # get 'comments/index'
+
+  root 'comments#welcom'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
