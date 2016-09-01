b238f10d438f40ee5a4dd4155afa72873dd66a26
Akhmedov Daniiar <lakiwolf.90@mail.ru>
2016-09-01 13:39:46 +0600

    Changed phrase second times in readme.txt

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+	validates :name, presence: true, length: {maximum: 50}
+
+	has_attached_file :image,
+		styles: {medium: '300x300>', thumb: '100x100>'},
+		default_url: '/images/:style/missing.png'
+
+		validates_attachment_content_type :image,
+			content_type: ['image/jpeg', 'image/gif', 'image/png']
+
+end
